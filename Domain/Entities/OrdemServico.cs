76e20b67331d6cb6af3a0529874b1490b066d17c
David <davidlarsonkonek0@gmail.com>
2026-05-06 11:44:33 -0300

    Ajustem em ordem serviço e seu service

--- Domain/Entities/OrdemServico.cs
+++ Domain/Entities/OrdemServico.cs
@@ -8,7 +8,7 @@ namespace Domain.Entities
     public class OrdemServico
     {
         public int Id { get; set; }
-        public string DataEntrada { get; set; } = string.Empty;
+        public DateTime DataEntrada { get; set; }
         public string Status { get; set; } = "Nova";
         public string Servico { get; set; } = string.Empty;
         public string Prioridade { get; set; } = "Normal";
@@ -33,5 +33,17 @@ namespace Domain.Entities
             Pagamento = pagamento;
             Status = "Pago";
         }
+
+        public void Abrir()
+        {
+            Status = "Aberta";
+            DataEntrada = DateTime.Now;
+        }
+
+        public void Finalizar()
+        {
+            Status = "Finalizada";
+        }
+
     }
 }
